cb3f8d1ffd76b4dce12abf87ffd530cd8aef606e
Jarizeelst <jari.mol@sintlucasedu.nl>
2025-09-25 14:11:30 +0200

    deney

--- js/myscript.js
+++ js/myscript.js
@@ -12,6 +12,17 @@ document.getElementById("acceptCookie").addEventListener("click", () => {
     popUp.classList.add("hide");
     popUp.classList.remove("show");
 });
+document.getElementById("deneyCookie").addEventListener("click", () => {
+    //Create date object
+    let d = new Date();
+    //Increment the current time by 1 minute (cookie will expire after 1 minute)
+    d.setDate(365 + d.getDay());
+    //Create Cookie withname = myCookieName, value = thisIsMyCookie and expiry time=1 minute
+    document.cookie = "myCookieName=thisIsMyCookie; expires = " + d + ";";
+    //Hide the popup
+    popUp.classList.add("hide");
+    popUp.classList.remove("show");
+});
 //Check if cookie is already present
 const checkCookie = () => {
     //Read the cookie and split on "="
